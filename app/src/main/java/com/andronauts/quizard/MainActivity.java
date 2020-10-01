a2425611a92cd4fb52f799e0470fba68dc9735c9
OmSingh <omsingh6903@gmail.com>
2020-10-01 18:48:42 +0530

    First Commit

---
+++ app/src/main/java/com/andronauts/quizard/MainActivity.java
@@ -0,0 +1,14 @@
+package com.andronauts.quizard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
