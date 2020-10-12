ba55080300ecc5ed91019b842e465b67c21709b7
OmSingh <omsingh6903@gmail.com>
2020-10-12 14:39:12 +0530

    created faculty layout

--- Android/app/src/main/java/com/andronauts/quizard/faculty/activities/HomeFacultyActivity.java
+++ Android/app/src/main/java/com/andronauts/quizard/faculty/activities/HomeFacultyActivity.java
@@ -1,16 +1,59 @@
 package com.andronauts.quizard.faculty.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 import com.andronauts.quizard.R;
+import com.andronauts.quizard.databinding.ActivityHomeFacultyBinding;
+import com.andronauts.quizard.general.activities.LoginActivity;
+import com.andronauts.quizard.utils.SessionManager;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeFacultyActivity extends AppCompatActivity {
-
+    ActivityHomeFacultyBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_faculty);
+        binding = ActivityHomeFacultyBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+        setSideNav();
+
+    }
+
+    private void setSideNav(){
+        binding.sideNav.navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.logout){
+                    new SessionManager(HomeFacultyActivity.this).logOutUser(new SessionManager.LogoutHandler() {
+                        @Override
+                        public void onSuccess() {
+                            Intent i = new Intent(HomeFacultyActivity.this, LoginActivity.class);
+                            startActivity(i);
+                            finish();
+                        }
+
+                        @Override
+                        public void onFailure() {
+
+                        }
+                    });
+                }
+
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        binding.drawer.openDrawer(Gravity.LEFT);
+        return super.onSupportNavigateUp();
     }
 }
--- Android/app/src/main/java/com/andronauts/quizard/students/activities/HomeStudentActivity.java
+++ Android/app/src/main/java/com/andronauts/quizard/students/activities/HomeStudentActivity.java
@@ -1,12 +1,19 @@
 package com.andronauts.quizard.students.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MenuItem;
 
 import com.andronauts.quizard.R;
 import com.andronauts.quizard.databinding.ActivityHomeStudentBinding;
+import com.andronauts.quizard.faculty.activities.HomeFacultyActivity;
+import com.andronauts.quizard.general.activities.LoginActivity;
+import com.andronauts.quizard.utils.SessionManager;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeStudentActivity extends AppCompatActivity {
     ActivityHomeStudentBinding binding;
@@ -17,12 +24,37 @@ public class HomeStudentActivity extends AppCompatActivity {
         binding = ActivityHomeStudentBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
-
+        setSideNav();
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         binding.drawer.openDrawer(Gravity.LEFT);
         return super.onSupportNavigateUp();
+    }
+
+    private void setSideNav(){
+        binding.sideNav.navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.logout){
+                    new SessionManager(HomeStudentActivity.this).logOutUser(new SessionManager.LogoutHandler() {
+                        @Override
+                        public void onSuccess() {
+                            Intent i = new Intent(HomeStudentActivity.this, LoginActivity.class);
+                            startActivity(i);
+                            finish();
+                        }
+
+                        @Override
+                        public void onFailure() {
+
+                        }
+                    });
+                }
+
+                return false;
+            }
+        });
     }
 }
